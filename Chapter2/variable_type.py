c17501449b322fa562e4e95ca7ae1072c011b123
rinkesh-gala <rinkeshgala1@gmail.com>
2024-12-14 17:16:42 +0530

    variable type program added

---
+++ Chapter2/variable_type.py
@@ -0,0 +1,13 @@
+#check the type of variable
+
+a = 10
+b= 10.5
+c= "Rinkesh"
+d= True
+e= None
+
+print("type of variable a is",type(a))
+print("type of variable b is",type(b))
+print("type of variable c is",type(c))
+print("type of variable d is",type(d))
+print("type of variable e is",type(e))
