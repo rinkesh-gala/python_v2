224b3fe532f9773c57ccf4003d1c85c31dea8b06
rinkesh-gala <rinkeshgala1@gmail.com>
2024-12-27 12:14:08 +0530

    file update

--- Chapter5/sets.py
+++ Chapter5/sets.py
@@ -1,6 +1,6 @@
 
 empty_dict = {} 
-empty_set = set() # to define set we use curly brackets 
+empty_set = set() # to define set we use curly brackets but to define empty set we have to do as mentioned 
 ex_set = {2,42,2,"Hello",42,6,3,"hello",True}
 print(type(empty_dict))
 print(type(empty_set))
