66b20b02a2d02b30037ad25bf696a6e34a3a8341
rinkesh-gala <rinkeshgala1@gmail.com>
2025-03-27 18:09:12 +0530

    fixed bug of converting to set

--- random_code/interview.py
+++ random_code/interview.py
@@ -11,8 +11,21 @@ def merge_sort(c):
                 c[static_index],c[moving_index]=c[moving_index],c[static_index]
     return c     
 
+def remove_dup(sorted_list):
+    dedup_list =[]
+    for item in sorted_list:
+        if item not in dedup_list:
+            dedup_list.append(item)
+    return dedup_list
+
+
+
+
 a=[1,4,3,5,8] 
-b=[2,4,5,9,8]  
+b=[2,4,5,9,8,-1]  
 sorted_list = merge_sort(a+b)
+#print(f"duplicate removed {list(set(sorted_list))}") # this doesn't work negative numbers. 
+# it gave output as [1, 2, 3, 4, 5, 8, 9, -1]
 print(f"{sorted_list=}")
-print(f"duplicate removed {list(set(sorted_list))}")
+print(f"dedup list: {remove_dup(sorted_list)}")
+
